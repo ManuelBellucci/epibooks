39a8537808f79de42da4cc3cd6001bc5d4d22f35
Manuel Bellucci <matiasbelluccimanuel@gmail.com>
2023-10-05 20:26:59 +0200

    fixed some css on navbar

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -13,19 +13,22 @@ export default function MyNav ({ query, setQuery }) {
   const { theme, toggleTheme } = useTheme()
 
   return (
-    <nav className={`fixed top-0 z-10 w-full p-5 border-solid border-b-2 border-black max-sm:flex max-sm:flex-col ${theme === 'dark' ? 'bg-slate-800' : 'bg-gray-300'} `}>
+    <nav
+      className={`fixed top-0 z-10 w-full p-5 border-b-2 border-solid border-black ${
+        theme === 'dark' ? 'bg-slate-800' : 'bg-gray-300'
+      } `}
+    >
       <header className='md:flex justify-between items-center w-full md:w-auto md:order-1 max-md:flex'>
         {/* Lista di navigazione */}
         <ul className='flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium max-md:hidden '>
-
           {/* Home */}
           <li className={`px-5 ${theme === 'dark' ? 'text-white' : ''}`}>
             <a
               href='/'
               className={`${
                 theme === 'dark'
-                  ? 'text-white hover:bg-gray-50 hover:text-slate-400 border-b border-gray-100 md:border-0 block pl-3 pr-4 py-2 md:p-2 rounded'
-                  : 'bg-blue-700 md:bg-transparent text-white block pl-3 pr-4 py-2 md:text-blue-700 md:p-2 rounded'
+                  ? 'text-white hover:text-slate-300 block pl-3 pr-4 py-2'
+                  : 'text-gray-700 hover:text-slate-500 block pl-3 pr-4 py-2'
               }`}
               aria-current='page'
             >
@@ -39,8 +42,8 @@ export default function MyNav ({ query, setQuery }) {
               href='/'
               className={`${
                 theme === 'dark'
-                  ? 'text-white hover:bg-gray-50 hover:text-slate-400 border-b border-gray-100 md:border-0 block pl-3 pr-4 py-2 md:p-2 rounded'
-                  : 'text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-2'
+                  ? 'text-white hover:text-slate-300 block pl-3 pr-4 py-2'
+                  : 'text-gray-700 hover:text-slate-500 block pl-3 pr-4 py-2'
               }`}
             >
               About
@@ -53,8 +56,8 @@ export default function MyNav ({ query, setQuery }) {
               href='/'
               className={`${
                 theme === 'dark'
-                  ? 'text-white hover:bg-gray-50 hover:text-slate-400 border-b border-gray-100 md:border-0 block pl-3 pr-4 py-2 md:p-2 rounded'
-                  : 'text-gray-700 border-b border-gray-100 md:hover-bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-2'
+                  ? 'text-white hover:text-slate-300 block pl-3 pr-4 py-2'
+                  : 'text-gray-700 hover:text-slate-500 block pl-3 pr-4 py-2'
               }`}
             >
               Browse
